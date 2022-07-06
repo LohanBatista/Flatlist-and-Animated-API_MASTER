fad18d519fda2739c2ec54a7215b0dc0feff26e1
Lohan Batista <lohan.batista@jointecnologia.com.br>
2022-07-06 09:20:39 -0300

    uuid and faker use

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 // Photo by Sharefaith from Pexels
 // Background image: https://www.pexels.com/photo/pink-rose-closeup-photography-1231265/
 
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import {
   StatusBar,
   FlatList,
@@ -17,16 +17,17 @@ import {
   SafeAreaView,
 } from 'react-native';
 const {width, height} = Dimensions.get('screen');
-import faker from 'faker';
+import {faker} from '@faker-js/faker';
+import uuidv4 from './src/config';
 
 faker.seed(10);
 const DATA = [...Array(30).keys()].map((_, i) => {
   return {
-    key: faker.random.uuid(),
-    image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
+    key: uuidv4(),
+    image: `https://randomuser.me/api/portraits/${faker.helpers.shuffle([
       'women',
       'men',
-    ])}/${faker.random.number(60)}.jpg`,
+    ])}/${faker.random.numeric(60)}.jpg`,
     name: faker.name.findName(),
     jobTitle: faker.name.jobTitle(),
     email: faker.internet.email(),
@@ -37,18 +38,12 @@ const SPACING = 20;
 const AVATAR_SIZE = 70;
 
 export default function App() {
+  useEffect(() => {
+    console.log(DATA);
+  }, []);
   return (
     <View style={{flex: 1, backgroundColor: '#fff'}}>
       <StatusBar hidden />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
